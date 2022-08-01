c9f66d7a8d6641be7534d2c8793383ae01585bd6
Luciano Gomes <luciano@pontotel.com.br>
2022-08-01 17:25:43 -0300

    feat(app/models/negociacoes.ts): add implementação de Negociacoes

---
+++ app/models/negociacoes.ts
@@ -0,0 +1,13 @@
+import { Negociacao } from "./negociacao";
+
+export class Negociacoes {
+    private negociacoes: Negociacao[] = [];
+
+    adicionaNegociacao(negociacao: Negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+
+    listarNegociacoes(): Negociacao[] {
+        return this.negociacoes;
+    }
+}
