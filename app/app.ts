c07632f8e3f28a97a2f77da1716ebb5dd588fa18
Luciano Gomes <luciano@pontotel.com.br>
2022-08-01 16:23:04 -0300

    feat(app/controllers/negociacao-controller.ts): implementação da controller de Negociação e integração com o formulário

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,8 @@
-import { Negociacao } from "./models/negociacao.js";
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
 
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao.volume);
+const controller = new NegociacaoController();
+const formulario = document.querySelector('.form');
+formulario.addEventListener('submit', (evento: Event) => {
+    evento.preventDefault();
+    controller.adiciona();
+})
