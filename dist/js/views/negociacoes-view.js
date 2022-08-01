62760bf02b79cd1b95e7500498058ccd907941be
Luciano Gomes <luciano@pontotel.com.br>
2022-08-01 19:34:39 -0300

    feat(src/app/*): renderizando lista de negociacoes através da NegociacoesView

--- dist/js/views/negociacoes-view.js
+++ dist/js/views/negociacoes-view.js
@@ -1,11 +1,8 @@
 export class NegociacoesView {
-    private elemento: HTMLElement;
-
-    constructor(seletor: string) {
+    constructor(seletor) {
         this.elemento = document.querySelector(seletor);
     }
-
-    template(): string {
+    template() {
         return `
         <table class="table table-hover table-bordered">
             <thead>
@@ -20,8 +17,7 @@ export class NegociacoesView {
         </table>
         `;
     }
-
-    update(): void {
+    update() {
         this.elemento.innerHTML = this.template();
     }
 }
